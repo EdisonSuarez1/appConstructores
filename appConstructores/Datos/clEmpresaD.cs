f95509f6d82e7e1f5952bf0ac09745945096a903
EdisonSuarez1 <easuarez94@misena.edu.co>
2022-04-22 19:02:39 -0500

    Select de Empresa ED

---
+++ appConstructores/Datos/clEmpresaD.cs
@@ -0,0 +1,41 @@
+﻿using appConstructores.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+
+namespace appConstructores.Datos
+{
+    public class clEmpresaD
+    {
+        public List<clEmpresaE> mtdListar()
+        {
+            string sql = "SELECT * FROM constructores";
+            clConexion objConexion = new clConexion();
+            DataTable tblEmpresa = new DataTable();
+            tblEmpresa = objConexion.mtdDesconectado(sql);
+
+            List<clEmpresaE> listaEmpresa = new List<clEmpresaE>();
+
+            int catReg = tblEmpresa.Rows.Count;
+
+            for (int i = 0; i < catReg; i++)
+            {
+
+                clEmpresaE objDatosEmpresa = new clEmpresaE();
+                objDatosEmpresa.idEmpresa= int.Parse(tblEmpresa.Rows[i]["idEmpresa"].ToString());
+                objDatosEmpresa.nombreempresa = tblEmpresa.Rows[i]["nombreempresa"].ToString();
+                objDatosEmpresa.direcciones = tblEmpresa.Rows[i]["direcciones"].ToString();
+                objDatosEmpresa.telefono = tblEmpresa.Rows[i]["telefono"].ToString();
+                objDatosEmpresa.ciudad = tblEmpresa.Rows[i]["ciudad"].ToString();
+              
+                listaEmpresa.Add(objDatosEmpresa);
+            }
+
+            return listaEmpresa;
+
+        }
+
+    }
+}
---
+++ appConstructores/Entidades/clEmpresaE.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace appConstructores.Entidades
+{
+    public class clEmpresaE
+    {
+        public int idEmpresa { get; set; }
+        public string nombreempresa { get; set; }
+        public string direcciones { get; set; }
+        public string telefono { get; set; }
+        public string ciudad { get; set; }
+
+
+
+    }
+}
